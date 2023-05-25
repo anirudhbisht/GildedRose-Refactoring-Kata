5fa905ea902ea16585d1abf67a905e9183986283
Anirudh Bisht <anirudhbisht@momomedical.com>
2023-05-25 15:45:18 +0200

    test: quality degrades 2x

--- TypeScript/test/jest/gilded-rose.spec.ts
+++ TypeScript/test/jest/gilded-rose.spec.ts
@@ -6,4 +6,12 @@ describe('Gilded Rose', () => {
     const items = gildedRose.updateQuality();
     expect(items[0].name).toBe('foo');
   });
+  it ('Once the sell by date has passed, Quality degrades twice as fast', () => {
+    const item = new Item('foo', 1, 100)
+    const gildedRose = new GildedRose([item])
+    let items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(99)    
+    items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(97)    
+  })
 });
