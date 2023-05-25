a45b517b323bc725c4042dcb9c72a58f8f4d2687
Anirudh Bisht <anirudhbisht@momomedical.com>
2023-05-25 17:03:28 +0200

    refactor: move behavior to separate classes

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -8,66 +8,89 @@ export class Item {
     this.sellIn = sellIn;
     this.quality = quality;
   }
+
+  updateQuality(): void {
+    this.quality -- 
+    if (this.sellIn <= 0) {
+      this.quality--
+    }
+    if (this.quality < 0) {
+      this.quality = 0
+    }
+    this.sellIn--
+  }
+}
+
+export class BrieItem extends Item {
+  constructor(sellIn: number, quality:number) {
+    super('Aged Brie', sellIn, quality)
+  }
+  updateQuality(): void {
+    this.quality += 1
+    this.sellIn -=1
+  }
+}
+
+export class BackstageItem extends Item {
+  constructor(sellIn: number, quality:number) {
+    super('Backstage passes to a TAFKAL80ETC concert', sellIn, quality)
+  }
+  updateQuality(): void {
+    if (this.sellIn > 10) {
+      this.quality += 1
+    } else if (this.sellIn > 5) {
+      this.quality += 2
+    } else if (this.sellIn > 3) {
+      this.quality += 3
+    } else if (this.sellIn <= 0) {
+      this.quality = 0
+    }
+    this.sellIn -=1
+  }
+}
+
+export class SulfurasItem  extends Item {
+  constructor(sellIn: number, quality: number) {
+    super('Sulfuras, Hand of Ragnaros', sellIn, quality)
+  }
+  updateQuality(): void {/** do nothing */}
+}
+
+export class ConjurItem  extends Item {
+  constructor(sellIn: number, quality: number) {
+    super('Conjured', sellIn, quality)
+  }
+  updateQuality(): void {
+    this.quality -= 2
+    this.sellIn -= 1
+  }
 }
 
 export class GildedRose {
   items: Array<Item>;
 
   constructor(items = [] as Array<Item>) {
-    this.items = items;
+    this.items = [];
+    for (const item of items) {
+
+      if (item.name === 'Aged Brie') {
+        this.items.push(new BrieItem(item.sellIn, item.quality))
+      } else if (item.name === 'Sulfuras, Hand of Ragnaros') {
+        this.items.push(new SulfurasItem(item.sellIn, item.quality))
+      } else if  (item.name === 'Conjured') {
+        this.items.push(new ConjurItem(item.sellIn, item.quality))
+      } else if  (item.name === 'Backstage passes to a TAFKAL80ETC concert') {
+        this.items.push(new BackstageItem(item.sellIn, item.quality))
+      } else {
+        this.items.push(item)
+      }
+    }
   }
 
   updateQuality() {
     for (let i = 0; i < this.items.length; i++) {
-      if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-        if (this.items[i].quality > 0) {
-          if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-            if (this.items[i].name != 'Conjured') {
-              this.items[i].quality = this.items[i].quality - 1
-            } else {
-              this.items[i].quality = this.items[i].quality - 2
-            }
-          }
-        }
-      } else {
-        if (this.items[i].quality < 50) {
-          this.items[i].quality = this.items[i].quality + 1
-          if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].sellIn < 11) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1
-              }
-            }
-            if (this.items[i].sellIn < 6) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1
-              }
-            }
-          }
-        }
-      }
-      if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-        this.items[i].sellIn = this.items[i].sellIn - 1;
-      }
-      if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != 'Aged Brie') {
-          if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].quality > 0) {
-              if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                this.items[i].quality = this.items[i].quality - 1
-              }
-            }
-          } else {
-            this.items[i].quality = this.items[i].quality - this.items[i].quality
-          }
-        } else {
-          if (this.items[i].quality < 50) {
-            this.items[i].quality = this.items[i].quality + 1
-          }
-        }
-      }
+      this.items[i].updateQuality()
     }
-
     return this.items;
   }
 }
--- TypeScript/test/jest/gilded-rose.spec.ts
+++ TypeScript/test/jest/gilded-rose.spec.ts
@@ -1,4 +1,60 @@
-import { Item, GildedRose } from '@/gilded-rose';
+import { Item, GildedRose, BrieItem, BackstageItem, SulfurasItem, ConjurItem } from '@/gilded-rose';
+
+describe ('AgedBrie', () => {
+  it('increases in Quality the older it gets', () => {
+    const brieItem  = new BrieItem(10, 20)
+    brieItem.updateQuality()
+    expect(brieItem.quality).toBeGreaterThan(20)
+    expect(brieItem.sellIn).toBe(9)
+  })
+})
+
+describe ('Backstage passes', () => {
+  it ('increases in Quality as its SellIn value approaches if SellIn more than 10 days', () => {
+    const backstageItem = new BackstageItem(12, 30)
+    backstageItem.updateQuality()
+    expect(backstageItem.quality).toBe(31)
+  })
+  it ('quality increases by 2 when there are 10 days or less', () => {
+    const backstageItem = new BackstageItem(10, 30)
+    backstageItem.updateQuality()
+    expect(backstageItem.quality).toBe(32)
+  })
+  it ('quality increases by 3 when there are 5 days or less', () => {
+    const backstageItem = new BackstageItem(5, 30)
+    backstageItem.updateQuality()
+    expect(backstageItem.quality).toBe(33)
+  })
+  it ('decreases to 0 when sellIn passes', () =>{
+    const backstageItem = new BackstageItem(0, 30)
+    backstageItem.updateQuality()
+    expect(backstageItem.quality).toBe(0)
+  })
+})
+
+
+describe ('Sulfuras', () => {
+  it('never has to be sold or decreases in Quality', () => {
+    const sellIn = 50
+    const quality = 50
+    const sulfurasItem = new SulfurasItem(sellIn, quality)
+    sulfurasItem.updateQuality()
+    expect(sulfurasItem.quality).toBe(quality)
+    expect(sulfurasItem.sellIn).toBe(sellIn)
+  })
+})
+
+
+describe ('Conjured', () => {
+  it('degrade in Quality twice as fast as normal items', () => {
+    const sellIn = 50
+    const quality = 50
+    const conjuredItem = new ConjurItem(sellIn, quality)
+    conjuredItem.updateQuality()
+    expect(conjuredItem.quality).toBe(quality - 2)
+    expect(conjuredItem.sellIn).toBe(sellIn - 1)
+  })
+})
 
 describe('Gilded Rose', () => {
   it('should foo', () => {
